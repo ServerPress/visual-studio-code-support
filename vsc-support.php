353282d991426ff1acb0f793508175979c3c31a2
Stephen J. Carnam <steve@steveorevo.com>
2019-06-21 18:34:12 -0700

    Shared function

--- vsc-support.php
+++ vsc-support.php
@@ -7,3 +7,23 @@ Author: Stephen Carnam
 Version: 1.0.0
 Author URI: http://steveorevo.com/
 */
+
+/**
+ * Create Visual Studio Code workspace directory. This function is invoked on
+ * site create, copy, move, import, and if it doesn't exist when the user
+ * clicks the VS Code button on localhost.
+ *
+ * @param $cwd The current working directory.
+ */
+function create_vsc_workspace($cwd) {
+
+    // TODO: create .vscode folder if it does not already exists
+    
+    // TODO: create/replace ds.code-workspace
+    // TODO: create/replace launch.json
+    // TODO: create/replace tasks.json
+    // TODO: create/replace xdebug.html
+    
+    // Update {{sitePath}} in ds.code-workspace
+    // Update {{siteName}} in tasks.json
+}
