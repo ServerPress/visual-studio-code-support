f1426a3688c28ce8a8fdd6c3d2f7b7d48b73f170
Stephen J. Carnam <steve@steveorevo.com>
2019-06-24 19:45:45 -0700

    implement create workspace folder

--- vsc-support.php
+++ vsc-support.php
@@ -17,13 +17,72 @@ Author URI: http://steveorevo.com/
  */
 function create_vsc_workspace($cwd) {
 
-    // TODO: create .vscode folder if it does not already exists
+    // Create .vscode folder if it does not already exist
+    if (file_exists($cwd . DIRECTORY_SEPARATOR . ".vscode") === FALSE) {
+        mkdir($cwd . DIRECTORY_SEPARATOR . ".vscode");
+    }
+
+    // Create or merge our launch file
+    $file = __DIR__ . DIRECTORY_SEPARATOR . "vscode" . DIRECTORY_SEPARATOR . "launch.json";
+    $temp = file_get_contents($file);
+    $file = $cwd . DIRECTORY_SEPARATOR . ".vscode" . DIRECTORY_SEPARATOR . "launch.json";
+    if (file_exists($file) === TRUE){
+        $launch = file_get_contents($file);
+        $launch = json_encode(array_merge(json_decode($launch, true),json_decode($temp, true)));
+    }else{
+        $launch = $temp;
+    }
+    file_put_contents($file, $launch);
+
+    // Prefix our platform specific vscode files
+    if ( PHP_OS === 'Darwin' ){
+        $prefix = __DIR__ . DIRECTORY_SEPARATOR . "vscode" . DIRECTORY_SEPARATOR . "mac-";
+    }else{
+        $prefix = __DIR__ . DIRECTORY_SEPARATOR . "vscode" . DIRECTORY_SEPARATOR . "win-";
+    } 
     
-    // TODO: create/replace ds.code-workspace
-    // TODO: create/replace launch.json
-    // TODO: create/replace tasks.json
-    // TODO: create/replace xdebug.html
-    
+    // Create or merge our workspace file
+    $file = $prefix . "ds.code-workspace";
+    $temp = file_get_contents($file);
+    $file = $cwd . DIRECTORY_SEPARATOR . ".vscode" . DIRECTORY_SEPARATOR . "ds.code-workspace";
+    if (file_exists($file) === TRUE){
+        $work = file_get_contents($file);
+        $work = json_encode(array_merge(json_decode($work, true),json_decode($temp, true)));
+    }else{
+        $work = $temp;
+    }
     // Update {{sitePath}} in ds.code-workspace
+    $x = str_replace("\\", "\\\\", $cwd);
+    $work = str_replace("{{sitePath}}", $x, $work);
+    file_put_contents($file, $work);
+
+    // Create or merge our tasks file
+    $file = $prefix . "tasks.json";
+    $temp = file_get_contents($file);
+    $file = $cwd . DIRECTORY_SEPARATOR . ".vscode" . DIRECTORY_SEPARATOR . "tasks.json";
+    if (file_exists($file) === TRUE){
+        $tasks = file_get_contents($file);
+        $tasks = json_encode(array_merge(json_decode($tasks, true),json_decode($temp, true)));
+    }else{
+        $tasks = $temp;
+    }
     // Update {{siteName}} in tasks.json
+    function getRightMost( $sSrc, $sSrch ) {
+        for ( $i = strlen( $sSrc ); $i >= 0; $i = $i - 1 ) {
+            $f = strpos( $sSrc, $sSrch, $i );
+            if ( $f !== false ) {
+                return substr( $sSrc, $f + strlen( $sSrch ), strlen( $sSrc ) );
+            }
+        }
+
+        return $sSrc;
+    }
+    $siteName = getRightMost($cwd, DIRECTORY_SEPARATOR);
+    $tasks = str_replace("{{siteName}}", $siteName, $tasks);
+    file_put_contents($file, $tasks);
+
+    // Copy over our xdebug.html redirect
+    copy(__DIR__ . DIRECTORY_SEPARATOR . "vscode" . DIRECTORY_SEPARATOR . "xdebug.html",
+        $cwd . DIRECTORY_SEPARATOR . ".vscode" . DIRECTORY_SEPARATOR . "xdebug.html");
 }
+
