8db055a95f40633d05e66accb5f332781f72af27
Stephen J. Carnam <steve@steveorevo.com>
2019-06-25 20:12:05 -0700

    Patch mac xdebug

--- prepend.php
+++ prepend.php
@@ -1,9 +1,16 @@
 <?php
-if (!isset($_SERVER['HTTP_USER_AGENT'])) return;
 
-// Generate .vscode folder and workspace settings
 global $ds_runtime;
 if ( $ds_runtime->last_ui_event !== false ) {
+
+	// Patch mac xdebug.so
+	if ( PHP_OS === 'Darwin' ){
+		if ( 'update_server' == $ds_runtime->last_ui_event->action ) {
+			copy(__DIR__ . "/vscode/xdebug.so", "/Applications/XAMPP/xamppfiles/lib/php/extensions/no-debug-non-zts-20180731/xdebug.so");
+		}
+	}
+	
+	// Generate .vscode folder and workspace settings
 	if ( strpos( "site_created,site_copied,site_moved,site_imported", $ds_runtime->last_ui_event->action ) !== false ) {
 		if ( strpos( "site_copied,site_moved", $ds_runtime->last_ui_event->action ) !== false ) {
 			$siteName = $ds_runtime->last_ui_event->info[1];
@@ -18,12 +25,13 @@ if ( $ds_runtime->last_ui_event !== false ) {
 			create_vsc_workspace($sitePath);
 		}
 	}
+	mytrace($ds_runtime->last_ui_event);
 }
+if (!isset($_SERVER['HTTP_USER_AGENT'])) return;
 
 // Add open in visual studio icon to localhost to open/start ds.code-workspace file
 $ds_runtime->add_action( 'domain_button_group', 'vsc_domain_button_group', 50 );
-function vsc_domain_button_group( $domain )
-{
+function vsc_domain_button_group( $domain ){
 	echo '<a href="http://localhost/ds-plugins/visual-studio-code-support/vsc-launch.php" data-domain="', $domain, '" style="background-color: #575297;border-color:#575297" class="btn btn-info dds-action vsc">VS Code</a>';
 }
 $ds_runtime->add_action( 'ds_footer', 'vsc_localhost_scripts' );
